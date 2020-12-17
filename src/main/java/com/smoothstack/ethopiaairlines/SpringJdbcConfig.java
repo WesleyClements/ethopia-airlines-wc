9a7580772c6baaecfd9891b682726f897fe4ce0b
Wesley Clements <wesley.clements@gmail.com>
2020-12-17 13:45:26 -0800

    added config to connect to MySQL database

---
+++ src/main/java/com/smoothstack/ethopiaairlines/SpringJdbcConfig.java
@@ -0,0 +1,20 @@
+package com.smoothstack.ethopiaairlines;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class SpringJdbcConfig {
+  @Bean
+  public DataSource mysqlDataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/utopia");
+    dataSource.setUsername("utopia");
+    dataSource.setPassword("password");
+    return dataSource;
+  }
+}
