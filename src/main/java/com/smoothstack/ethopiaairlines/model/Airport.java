0da23495ddbfd3aedcc889d9582dbefc0bf75d61
Wesley Clements <wesley.clements@gmail.com>
2020-12-17 13:45:38 -0800

    added Airport model

---
+++ src/main/java/com/smoothstack/ethopiaairlines/model/Airport.java
@@ -0,0 +1,23 @@
+package com.smoothstack.ethopiaairlines.model;
+
+public class Airport {
+  String iataId;
+
+  String city;
+
+  public String getIataId() {
+    return this.iataId;
+  }
+
+  public void setIataId(final String value) {
+    this.iataId = value;
+  }
+
+  public String getCity() {
+    return this.city;
+  }
+
+  public void setCity(final String value) {
+    this.city = value;
+  }
+}
