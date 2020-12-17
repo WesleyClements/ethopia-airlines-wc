cedb1fb288c2a432ff7a57c7a7fe0675092f84bc
Wesley Clements <wesley.clements@gmail.com>
2020-12-17 11:09:39 -0800

    initial commit

---
+++ src/main/java/com/smoothstack/ethopiaairlines/EthopiaAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.ethopiaairlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthopiaAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthopiaAirlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothstack/ethopiaairlines/EthopiaAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.ethopiaairlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthopiaAirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
