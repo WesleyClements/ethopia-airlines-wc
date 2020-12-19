5fcef3a1d75a26306c01bbcf76e4ad80ba3fb920
Wesley Clements <wesley.clements@gmail.com>
2020-12-19 10:09:55 -0800

    added content negotiation config

---
+++ src/main/java/com/smoothstack/uthopia/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.smoothstack.uthopia.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+  @Override
+  public void configureContentNegotiation(final ContentNegotiationConfigurer config) {
+    config.favorParameter(false).parameterName("format").ignoreAcceptHeader(false).useRegisteredExtensionsOnly(true);
+    config.defaultContentType(MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML).mediaType("json",
+        MediaType.APPLICATION_JSON);
+  }
+}
