72ea6ea4569e6ab9c9afa776f5416e8e1c056978
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 09:50:31 -0800

    removed unused import

--- src/main/java/com/smoothstack/uthopia/dao/AirportDAO.java
+++ src/main/java/com/smoothstack/uthopia/dao/AirportDAO.java
@@ -1,7 +1,5 @@
 package com.smoothstack.uthopia.dao;
 
-import java.util.List;
-
 import com.smoothstack.uthopia.model.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
