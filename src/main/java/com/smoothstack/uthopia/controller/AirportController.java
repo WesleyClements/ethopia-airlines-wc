636d4a780435867ba7a2cb664488a987fb02b332
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 09:16:16 -0800

    added service layer

--- src/main/java/com/smoothstack/uthopia/controller/AirportController.java
+++ src/main/java/com/smoothstack/uthopia/controller/AirportController.java
@@ -1,12 +1,9 @@
 package com.smoothstack.uthopia.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import javax.persistence.EntityNotFoundException;
-
-import com.smoothstack.uthopia.dao.AirportDAO;
 import com.smoothstack.uthopia.model.Airport;
+import com.smoothstack.uthopia.service.AirportService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,24 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/airports")
 public class AirportController {
   @Autowired
-  private AirportDAO airportDAO;
+  private AirportService airportService;
 
   @GetMapping("")
   public @ResponseBody List<Airport> getAirports() {
-    return airportDAO.findAll();
+    return airportService.findAll();
   }
 
   @GetMapping("/{id}")
-  public @ResponseBody Airport getAirportById(@PathVariable final String id) throws Exception {
-    final Optional<Airport> optional = airportDAO.findById(id);
-    if (!optional.isPresent())
-      throw new EntityNotFoundException("cannot find airport with id: " + id);
-    return optional.get();
+  public @ResponseBody Airport getAirportById(@PathVariable final String id) {
+    return airportService.findById(id);
   }
 
   @PostMapping("")
   public @ResponseBody Airport createAirport(@RequestBody final Airport airport) {
-    airportDAO.save(airport);
-    return airport;
+    return airportService.create(airport);
   }
 }
