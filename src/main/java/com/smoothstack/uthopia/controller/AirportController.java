e179c81c0c4d5dbedb643777e3a91c9ae47b274d
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 08:33:38 -0800

    changed to throw error if there is no airport with that id

--- src/main/java/com/smoothstack/uthopia/controller/AirportController.java
+++ src/main/java/com/smoothstack/uthopia/controller/AirportController.java
@@ -3,19 +3,21 @@ package com.smoothstack.uthopia.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.smoothstack.uthopia.dao.AirportDAO;
 import com.smoothstack.uthopia.model.Airport;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/airports")
 public class AirportController {
   @Autowired
@@ -27,8 +29,11 @@ public class AirportController {
   }
 
   @GetMapping("/{id}")
-  public @ResponseBody Optional<Airport> getAirportById(@PathVariable final String id) {
-    return airportDAO.findById(id);
+  public @ResponseBody Airport getAirportById(@PathVariable final String id) throws Exception {
+    final Optional<Airport> optional = airportDAO.findById(id);
+    if (!optional.isPresent())
+      throw new EntityNotFoundException();
+    return optional.get();
   }
 
   @PostMapping("")
