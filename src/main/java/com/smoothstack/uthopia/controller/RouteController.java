e734be875bd061d7b9e36c2ce965a290796726bf
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 09:49:56 -0800

    updated to use service layer

--- src/main/java/com/smoothstack/uthopia/controller/RouteController.java
+++ src/main/java/com/smoothstack/uthopia/controller/RouteController.java
@@ -1,12 +1,12 @@
 package com.smoothstack.uthopia.controller;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
-import javax.persistence.EntityNotFoundException;
-
-import com.smoothstack.uthopia.dao.RouteDAO;
+import com.smoothstack.exception.BadRequestException;
 import com.smoothstack.uthopia.model.Route;
+import com.smoothstack.uthopia.model.RouteDTO;
+import com.smoothstack.uthopia.service.RouteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,24 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/routes")
 public class RouteController {
   @Autowired
-  private RouteDAO routeDAO;
+  private RouteService routeService;
 
   @GetMapping("")
-  public @ResponseBody List<Route> getRoutes() {
-    return routeDAO.findAll();
+  public @ResponseBody List<Route> getRoutes(@RequestParam final Map<String, String> params) {
+    final String origin = params.get("origin");
+    final String destination = params.get("destination");
+    if (origin == null && destination == null)
+      return routeService.findAll();
+    else
+      return routeService.findAllWithOriginAndDestination(origin, destination);
   }
 
   @GetMapping("/{id}")
-  public @ResponseBody Route getRouteById(@PathVariable final Integer id) throws Exception {
-    final Optional<Route> optional = routeDAO.findById(id);
-    if (!optional.isPresent())
-      throw new EntityNotFoundException("cannot find route with id: " + id);
-    return optional.get();
+  public @ResponseBody Route getRouteById(@PathVariable final Integer id) {
+    return routeService.findById(id);
   }
 
   @PostMapping("")
-  public @ResponseBody Route getRouteById(@RequestBody final Route route) {
-    routeDAO.save(route);
-    return route;
+  public @ResponseBody Route getRouteById(@RequestBody final RouteDTO routeDTO) throws BadRequestException {
+    return routeService.create(routeDTO);
   }
 }
