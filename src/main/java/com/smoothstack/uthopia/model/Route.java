3a7178c4bdc2b648b59dac747cea8dd6d10dc8df
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 08:28:14 -0800

    added route model

---
+++ src/main/java/com/smoothstack/uthopia/model/Route.java
@@ -0,0 +1,32 @@
+package com.smoothstack.uthopia.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "route")
+public class Route {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "route_id")
+  private Integer routeId;
+
+  @ManyToOne
+  @JoinColumn(name = "origin_airport_id", nullable = false)
+  private Airport origin;
+
+  @ManyToOne
+  @JoinColumn(name = "destination_airport_id", nullable = false)
+  private Airport destination;
+
+  public Integer getRouteId() {
+    return routeId;
+  }
+
+}
