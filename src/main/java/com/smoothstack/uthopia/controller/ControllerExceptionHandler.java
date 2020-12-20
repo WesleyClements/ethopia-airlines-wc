57c012ac70555e00ff3e9ff526a60d9feca5e9c0
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 09:49:31 -0800

    added handler for badrequest exception

--- src/main/java/com/smoothstack/uthopia/controller/ControllerExceptionHandler.java
+++ src/main/java/com/smoothstack/uthopia/controller/ControllerExceptionHandler.java
@@ -2,6 +2,8 @@ package com.smoothstack.uthopia.controller;
 
 import javax.persistence.EntityNotFoundException;
 
+import com.smoothstack.exception.BadRequestException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +14,10 @@ public class ControllerExceptionHandler {
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(EntityNotFoundException.class)
   public void handleEntityNotFound() {
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(BadRequestException.class)
+  public void handleBadRequest() {
   }
 }
