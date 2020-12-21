b1dfa54d6e4d7a48f60dfe659d65856ae379cf13
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 17:31:19 -0800

    added flight model

---
+++ src/main/java/com/smoothstack/uthopia/model/Flight.java
@@ -0,0 +1,67 @@
+package com.smoothstack.uthopia.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "flight")
+public class Flight {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "flight_id")
+  private Integer flightId;
+
+  @Column(name = "route_id")
+  private Integer routeId;
+
+  @Column(name = "departure_time")
+  private LocalDateTime departureTime;
+
+  @Column(name = "capacity")
+  private Integer capacity;
+
+  @Column(name = "capacity")
+  private Double seatPrice;
+
+  public Integer getFlightId() {
+    return flightId;
+  }
+
+  public Integer getRouteId() {
+    return routeId;
+  }
+
+  public void setRouteId(final Integer routeId) {
+    this.routeId = routeId;
+  }
+
+  public LocalDateTime getDepartureTime() {
+    return departureTime;
+  }
+
+  public void setDepartureTime(final LocalDateTime departureTime) {
+    this.departureTime = departureTime;
+  }
+
+  public Integer getCapacity() {
+    return capacity;
+  }
+
+  public void setCapacity(final Integer capacity) {
+    this.capacity = capacity;
+  }
+
+  public Double getSeatPrice() {
+    return seatPrice;
+  }
+
+  public void setSeatPrice(final Double seatPrice) {
+    this.seatPrice = seatPrice;
+  }
+}
