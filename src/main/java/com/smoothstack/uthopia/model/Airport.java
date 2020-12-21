b8e305d5f4240316e53771dc0ab566afc619c2e2
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 20:22:25 -0800

    aded route airport connections

--- src/main/java/com/smoothstack/uthopia/model/Airport.java
+++ src/main/java/com/smoothstack/uthopia/model/Airport.java
@@ -1,10 +1,17 @@
 package com.smoothstack.uthopia.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @XmlRootElement
 @Entity
@@ -12,10 +19,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Airport {
   @Id
   @Column(name = "iata_id")
-  String iataId;
+  private String iataId;
 
   @Column(name = "city")
-  String city;
+  private String city;
+
+  @JsonManagedReference
+  @OneToMany(mappedBy = "origin", fetch = FetchType.LAZY)
+  private Set<Route> routes = new HashSet<>();
 
   public String getIataId() {
     return this.iataId;
@@ -31,5 +42,13 @@ public class Airport {
 
   public void setCity(final String value) {
     this.city = value;
+  }
+
+  public Set<Route> getRoutes() {
+    return routes;
+  }
+
+  public void setRoutes(final Set<Route> routes) {
+    this.routes = routes;
   }
 }
--- src/main/java/com/smoothstack/uthopia/model/Route.java
+++ src/main/java/com/smoothstack/uthopia/model/Route.java
@@ -2,45 +2,80 @@ package com.smoothstack.uthopia.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @XmlRootElement
 @Entity
 @Table(name = "route")
 public class Route {
+  @JsonIgnore
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "route_id")
   private Integer routeId;
 
-  @Column(name = "origin_id")
-  private String origin;
+  @JsonProperty("origin")
+  @Column(name = "origin_id", insertable = false, updatable = false)
+  private String originId;
 
-  @Column(name = "destination_id")
-  private String destination;
+  @JsonIgnore
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "origin_id")
+  private Airport origin;
+
+  @JsonProperty("destination")
+  @Column(name = "destination_id", insertable = false, updatable = false)
+  private String destinationId;
+
+  @JsonIgnore
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "destination_id")
+  private Airport destination;
 
   public Integer getRouteId() {
     return routeId;
   }
 
-  public String getOrigin() {
+  public Airport getOrigin() {
     return origin;
   }
 
-  public void setOrigin(final String origin) {
+  public void setOrigin(final Airport origin) {
     this.origin = origin;
   }
 
-  public String getDestination() {
+  public String getOriginId() {
+    return originId;
+  }
+
+  public void setOriginId(final String originId) {
+    this.originId = originId;
+  }
+
+  public Airport getDestination() {
     return destination;
   }
 
-  public void setDestination(final String destination) {
+  public void setDestination(final Airport destination) {
     this.destination = destination;
+  }
+
+  public String getDestinationId() {
+    return destinationId;
+  }
+
+  public void setDestinationId(final String destinationId) {
+    this.destinationId = destinationId;
   }
 
 }
