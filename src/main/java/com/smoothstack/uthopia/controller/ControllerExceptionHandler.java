8839069ad837d96fa836d3aeae30936e82c8f096
Wesley Clements <wesley.clements@gmail.com>
2020-12-21 11:02:50 -0800

    added response body for state conflict

--- src/main/java/com/smoothstack/uthopia/controller/ControllerExceptionHandler.java
+++ src/main/java/com/smoothstack/uthopia/controller/ControllerExceptionHandler.java
@@ -2,10 +2,12 @@ package com.smoothstack.uthopia.controller;
 
 import com.smoothstack.uthopia.exception.BadRequestException;
 import com.smoothstack.uthopia.exception.NotFoundException;
+import com.smoothstack.uthopia.exception.StateConflictException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -21,32 +23,44 @@ public class ControllerExceptionHandler {
 
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(NotFoundException.class)
-  public void handleEntityNotFound() {
+  public void handleEntityNotFound(final Exception e) {
+    logger.error(e.getClass().toString());
   }
 
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler({ BadRequestException.class, HttpMessageNotReadableException.class })
-  public void handleBadRequest() {
+  public void handleBadRequest(final Exception e) {
+    logger.error(e.getClass().toString());
+  }
+
+  @ResponseStatus(HttpStatus.CONFLICT)
+  @ExceptionHandler(StateConflictException.class)
+  public ResponseEntity<String> handleConflict(final Exception e) {
+    logger.error(e.getClass().toString());
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
   }
 
   @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-  public void handleMethodNotAllowed() {
+  public void handleMethodNotAllowed(final Exception e) {
+    logger.error(e.getClass().toString());
   }
 
   @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
   @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-  public void handleNotAcceptableMediaType() {
+  public void handleNotAcceptableMediaType(final Exception e) {
+    logger.error(e.getClass().toString());
   }
 
   @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
   @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-  public void handleUnsupportedMediaType() {
+  public void handleUnsupportedMediaType(final Exception e) {
+    logger.error(e.getClass().toString());
   }
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
   public void handle(final Exception e) {
-    logger.error(e.getMessage(), e.getClass());
+    logger.error(e.getClass().toString());
   }
 }
