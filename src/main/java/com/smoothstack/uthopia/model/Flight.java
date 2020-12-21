e684d9e07eefe67b963ee47a553f4c2b41a0d89c
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 21:45:12 -0800

    added departure date view

--- src/main/java/com/smoothstack/uthopia/model/Flight.java
+++ src/main/java/com/smoothstack/uthopia/model/Flight.java
@@ -1,5 +1,6 @@
 package com.smoothstack.uthopia.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
@@ -36,6 +37,10 @@ public class Flight {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "route_id")
   private Route route;
+
+  @JsonIgnore
+  @Column(name = "departure_time", insertable = false, updatable = false)
+  private LocalDate departureDate;
 
   @JsonProperty("departure")
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Etc/GMT")
@@ -82,6 +87,10 @@ public class Flight {
 
   public String getDestination() {
     return this.route.getDestinationId();
+  }
+
+  public LocalDate getDepartureDate() {
+    return departureDate;
   }
 
   public LocalDateTime getDepartureTime() {
