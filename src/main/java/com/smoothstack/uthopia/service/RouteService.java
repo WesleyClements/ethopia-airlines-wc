3838ad390f9e0d7381dc1dde34adc764cf13e5ba
Wesley Clements <wesley.clements@gmail.com>
2020-12-20 17:25:08 -0800

    fixed the logic

--- src/main/java/com/smoothstack/uthopia/service/RouteService.java
+++ src/main/java/com/smoothstack/uthopia/service/RouteService.java
@@ -5,20 +5,14 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
-import com.smoothstack.exception.BadRequestException;
-import com.smoothstack.uthopia.dao.AirportDAO;
 import com.smoothstack.uthopia.dao.RouteDAO;
-import com.smoothstack.uthopia.model.Airport;
 import com.smoothstack.uthopia.model.Route;
-import com.smoothstack.uthopia.model.RouteDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RouteService {
-  @Autowired
-  private AirportDAO airportDAO;
   @Autowired
   private RouteDAO routeDAO;
 
@@ -28,11 +22,11 @@ public class RouteService {
 
   public List<Route> findAllWithOriginAndDestination(final String origin, final String destination) {
     if (destination == null)
-      return routeDAO.findByOriginIataId(origin);
+      return routeDAO.findAllByOrigin(origin);
     else if (origin == null)
-      return routeDAO.findByDestinationIataId(destination);
+      return routeDAO.findAllByDestination(destination);
     else
-      return routeDAO.findByOriginIataIdAndDestinationIataId(origin, destination);
+      return routeDAO.findAllByOriginAndDestination(origin, destination);
   }
 
   public Route findById(final Integer id) {
@@ -42,16 +36,7 @@ public class RouteService {
     return optional.get();
   }
 
-  public Route create(final RouteDTO routeDTO) throws BadRequestException {
-    final Optional<Airport> originAirport = airportDAO.findById(routeDTO.getOrigin());
-    if (!originAirport.isPresent())
-      throw new BadRequestException("invalid origin airport: " + routeDTO.getOrigin());
-    final Optional<Airport> destinationAirport = airportDAO.findById(routeDTO.getOrigin());
-    if (!destinationAirport.isPresent())
-      throw new BadRequestException("invalid destination airport: " + routeDTO.getOrigin());
-    final Route route = new Route();
-    route.setOrigin(originAirport.get());
-    route.setDestination(destinationAirport.get());
+  public Route create(final Route route) {
     routeDAO.save(route);
     return route;
   }
