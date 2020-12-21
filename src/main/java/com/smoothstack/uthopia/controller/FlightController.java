ce3153317b2a4c2060db338ccff776dc550c0f2e
Wesley Clements <wesley.clements@gmail.com>
2020-12-21 08:47:16 -0800

    added delete route for flight

--- src/main/java/com/smoothstack/uthopia/controller/FlightController.java
+++ src/main/java/com/smoothstack/uthopia/controller/FlightController.java
@@ -12,6 +12,7 @@ import com.smoothstack.uthopia.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,12 @@ public class FlightController {
   @GetMapping("/{id}")
   public @ResponseBody Flight getFlightById(@PathVariable final Integer id) throws NotFoundException {
     return flightService.findById(id);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> deleteRoute(@PathVariable final Integer id) throws NotFoundException {
+    flightService.delete(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
 }
