e03ed8390dea917f8acdb3835e535682c3f10aaa
Wesley Clements <wesley.clements@gmail.com>
2020-12-24 08:05:57 -0800

    added tests for finds

--- src/test/java/com/smoothstack/ethopiaairlines/service/RouteServiceTest.java
+++ src/test/java/com/smoothstack/ethopiaairlines/service/RouteServiceTest.java
@@ -1,10 +1,15 @@
 package com.smoothstack.ethopiaairlines.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import com.smoothstack.uthopia.UthopiaAirlinesApplication;
 import com.smoothstack.uthopia.dao.RouteDAO;
+import com.smoothstack.uthopia.exception.NotFoundException;
 import com.smoothstack.uthopia.model.Route;
 import com.smoothstack.uthopia.service.RouteService;
 import org.junit.jupiter.api.Test;
@@ -26,18 +31,21 @@ public class RouteServiceTest {
     final List<Route> routes = new ArrayList<Route>();
     {
       Route route = new Route();
+      route.setRouteId(1);
       route.setOriginId("AAA");
       route.setOriginId("AAB");
       routes.add(route);
     }
     {
       Route route = new Route();
+      route.setRouteId(2);
       route.setOriginId("AAC");
       route.setOriginId("AAB");
       routes.add(route);
     }
     {
       Route route = new Route();
+      route.setRouteId(3);
       route.setOriginId("AAA");
       route.setOriginId("AAC");
       routes.add(route);
@@ -56,8 +64,47 @@ public class RouteServiceTest {
 
   @Test
   void getAllRoutesByOriginAndDestination() throws Exception {
+    final String origin = "AAA";
+    final String destination = "AAB";
     final List<Route> routes = getRoutes();
+    final List<Route> originFilteredRoutes = routes.stream()
+        .filter((route) -> route.getOriginId() == origin).collect(Collectors.toList());
+    final List<Route> destinationFilteredRoutes = routes.stream()
+        .filter((route) -> route.getDestinationId() == destination).collect(Collectors.toList());
+    final List<Route> originAndDestinationFilteredRoutes = routes.stream()
+        .filter((route) -> route.getOriginId() == origin && route.getDestinationId() == destination)
+        .collect(Collectors.toList());
     Mockito.when(routeDAO.findAll()).thenReturn(routes);
-    assertIterableEquals(routes, routeService.findAll());
+    Mockito.when(routeDAO.findAllByOriginId(origin)).thenReturn(originFilteredRoutes);
+    Mockito.when(routeDAO.findAllByDestinationId(destination))
+        .thenReturn(destinationFilteredRoutes);
+    Mockito.when(routeDAO.findAllByOriginIdAndDestinationId(origin, destination))
+        .thenReturn(originAndDestinationFilteredRoutes);
+
+    assertIterableEquals(routes, routeService.findAllWithOriginAndDestination(null, null));
+    assertIterableEquals(originFilteredRoutes,
+        routeService.findAllWithOriginAndDestination(origin, null));
+    assertIterableEquals(destinationFilteredRoutes,
+        routeService.findAllWithOriginAndDestination(null, destination));
+    assertIterableEquals(originAndDestinationFilteredRoutes,
+        routeService.findAllWithOriginAndDestination(origin, destination));
+  }
+
+  @Test
+  void findById() throws NotFoundException {
+    final Integer id = 2;
+    final Integer nonExistentId = 4;
+    final Route routeWithId = getRoutes().stream().filter(route -> route.getRouteId() == id)
+        .collect(Collectors.toList()).get(0);
+    System.out.println(routeWithId.getRouteId());
+    Mockito.when(routeDAO.findById(id)).thenReturn(Optional.of(routeWithId));
+    Mockito.when(routeDAO.findById(nonExistentId)).thenReturn(Optional.empty());
+    assertEquals(routeWithId, routeService.findById(id));
+    assertThrows(NotFoundException.class, () -> routeService.findById(nonExistentId));
+  }
+
+  @Test
+  void createRoute() {
+
   }
 }
