cd355040c38e72ee64efd6564d3c5b5d6de08895
Wesley Clements <wesley.clements@gmail.com>
2020-12-23 18:24:54 -0800

    fixed tests

--- src/test/java/com/smoothstack/ethopiaairlines/UthopiaAirlinesApplicationTests.java
+++ src/test/java/com/smoothstack/ethopiaairlines/UthopiaAirlinesApplicationTests.java
@@ -1,13 +1,12 @@
 package com.smoothstack.ethopiaairlines;
 
+import com.smoothstack.uthopia.UthopiaAirlinesApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UthopiaAirlinesApplication.class)
 class UthopiaAirlinesApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
